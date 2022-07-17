14a7b31e0d2b2e2fac5cea83c9766453262f98c1
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-07-18 00:03:33 +0700

    Updates

---
+++ precache-manifest.d3d4dd3a7f1c2efa77b5a26dce057ef0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9ef2f9399981babb1c",
+    "url": "/portfolio/static/css/main.36c0cad0.chunk.css"
+  },
+  {
+    "revision": "2c9ef2f9399981babb1c",
+    "url": "/portfolio/static/js/main.2c9ef2f9.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/portfolio/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "e33e6d9d2cf5f9a4e844",
+    "url": "/portfolio/static/js/2.e33e6d9d.chunk.js"
+  },
+  {
+    "revision": "c79570d36e2f95c56abb",
+    "url": "/portfolio/static/js/runtime~main.c79570d3.js"
+  },
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/portfolio/static/media/logo.26ef1cbc.svg"
+  },
+  {
+    "revision": "d055525d382ba31cea13162363773784",
+    "url": "/portfolio/static/media/simpeo.d055525d.jpg"
+  },
+  {
+    "revision": "e3c01f28081a8f1328f213803b7e6bb0",
+    "url": "/portfolio/index.html"
+  }
+];
