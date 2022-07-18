6413f8af6ae4c427a0395e53e3b5d4e1268fdde5
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-07-18 09:17:58 +0700

    Updates

--- precache-manifest.2637363cdbf1f9e58ce5ba1f3654e623.js
+++ precache-manifest.2637363cdbf1f9e58ce5ba1f3654e623.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "3c43e1f88d276f863ecd",
+    "revision": "22665301388de1034848",
     "url": "/portfolio/static/css/main.332ea6ac.chunk.css"
   },
   {
-    "revision": "3c43e1f88d276f863ecd",
-    "url": "/portfolio/static/js/main.3c43e1f8.chunk.js"
+    "revision": "22665301388de1034848",
+    "url": "/portfolio/static/js/main.22665301.chunk.js"
   },
   {
     "revision": "836e4380fe3f7816a983",
     "url": "/portfolio/static/js/1.836e4380.chunk.js"
   },
   {
-    "revision": "e33e6d9d2cf5f9a4e844",
-    "url": "/portfolio/static/js/2.e33e6d9d.chunk.js"
+    "revision": "beaa700ee85de8d407cc",
+    "url": "/portfolio/static/js/2.beaa700e.chunk.js"
   },
   {
     "revision": "476adc593e4f9caead89",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/simpeo.d055525d.jpg"
   },
   {
-    "revision": "929b65da4852cc101d3b901bdacb5d5d",
+    "revision": "54e1d4691fc31436190fcde9c69f4195",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5c08753b122d6c180eec39418d02bb20.js"
+  "/portfolio/precache-manifest.2637363cdbf1f9e58ce5ba1f3654e623.js"
 );
 
 workbox.clientsClaim();
